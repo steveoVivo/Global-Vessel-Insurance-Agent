22c923f08a2714cda78bc8b321e3961a4875f083
Steven Shoemaker <stshoema@gmail.com>
2026-05-12 22:16:54 -0700

    Added Openlayers Dependency with Map that starts in UC Davis

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,8 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
+import { Map, View } from 'ol'
+import { OSM } from 'ol/source'
+import TileLayer from 'ol/layer/Tile'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -6,21 +10,51 @@ import './App.css'
 
 const starterText = "If you see this message, your React app is working. This message should be replaced momentarily.";
 
+const ucDavisCoordinates = [-93703952.94088145, 4656009.537393207];
+const ucDavisZoom = 14.165;
+
 function App() {
-  // TODO: Type the useState once you find out why templates are being so weird
+  // TODO: The following 3 should use templates `HTMLDivElement, string, Map`.
+  //   For some reason it's whining when I use em
+  const mapRef = useRef(null);
+
   const [data, setData] = useState(starterText);
+  const [map, setMap] = useState(null)
 
   useEffect(() => {
-      // '/api' retrieves data from the Vite Proxy
-      fetch('/api/data')
-        .then(res => res.json())
-        .then(data => setData(data.message))
-        .catch(err => console.error(err))
-    }, []);
+    // -------> Retrieve Test Data <-------
+
+    // '/api' retrieves data from the Vite Proxy
+    fetch('/api/data')
+      .then(res => res.json())
+      .then(data => setData(data.message))
+      .catch(err => console.error(err))
+
+    // -------> Retrieve Map Data <-------
+    const openLayersMap = new Map({
+      target: mapRef.current,
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: ucDavisCoordinates,
+        zoom: ucDavisZoom
+      })
+    });
+
+    // TODO: For some reason, this renders a second map
+    // setMap(openLayersMap);
+
+    // Un-Render the map when App is un-rendered
+    return () => openLayersMap.setTarget(undefined);
+  }, []);
 
   return (
-    <div>
-      {data}
+    <div style={{display: 'grid', placeItems: 'center'}}>
+      <div> {data} </div>
+      <div ref={mapRef} style={{width: '90%', height: 600}}></div>
     </div>
   )
 }
