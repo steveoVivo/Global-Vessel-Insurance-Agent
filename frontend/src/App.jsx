6d6eaca794b7934b3d13e83a9e1615ac0a93ad17
Steven Shoemaker <stshoema@gmail.com>
2026-05-21 12:35:52 -0700

    Resize elements with CSS, add test circle images to the map in the Bay Area

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
-import { useState, useEffect, useRef } from 'react'
-import { Map, View } from 'ol'
-import { OSM } from 'ol/source'
-import TileLayer from 'ol/layer/Tile'
+import { useState, useEffect, useRef } from 'react';
+import { Map, View } from 'ol';
+import { OSM } from 'ol/source';
+import TileLayer from 'ol/layer/Tile';
 
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import reactLogo from './assets/react.svg';
+import viteLogo from './assets/vite.svg';
+import heroImg from './assets/hero.png';
+import './App.css';
+
+// Necessary to import in the same place as the map, otherwise controls will look weird
+import 'ol/ol.css';
+// Imports that should eventually be moved into a functionality file
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import { fromLonLat } from 'ol/proj';
+import { Style, Circle, Fill, Stroke } from 'ol/style';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+
 
 const starterText = "If you see this message, your React app is working. This message should be replaced momentarily.";
 
@@ -23,14 +34,14 @@ function App() {
 
   useEffect(() => {
     // -------> Retrieve Test Data <-------
-
     // '/api' retrieves data from the Vite Proxy
     fetch('/api/data')
       .then(res => res.json())
       .then(data => setData(data.message))
       .catch(err => console.error(err))
 
-    // -------> Retrieve Map Data <-------
+
+    // -------> Create the map <-------
     const openLayersMap = new Map({
       target: mapRef.current,
       layers: [
@@ -44,8 +55,47 @@ function App() {
       })
     });
 
-    // TODO: For some reason, this renders a second map
-    // setMap(openLayersMap);
+
+    // -------> TEST: Render circles on the map <-------
+    // TODO: Move this to the top of the useEffect
+    if (!mapRef.current) return;
+
+    // SF, Oakland, and Twin Peaks. I got the exact co-ordinates from Google Gemini
+    const testCoordinates = [
+      [-122.4194, 37.7749],
+      [-122.2711, 37.8044],
+      [-122.4467, 37.7337]
+    ];
+
+    // TODO: Is this the best way of doing this? Not a huge fan of literally drawing images
+    const testCircles = testCoordinates.map(coordinate => {
+      return new Feature({
+        geometry: new Point(fromLonLat(coordinate))
+      });
+    });
+
+    const circleStyles = new Style({
+      image: new Circle({
+        radius: 20,
+        fill: new Fill({ color: 'rgba(160, 0, 0, 0.65)' }),
+        stroke: new Stroke({ color: 'rgb(160, 0, 0)', width: 3 })
+      })
+    });
+
+    const circleSource = new VectorSource({
+      features: testCircles,
+    });
+
+    const circleLayer = new VectorLayer({
+      source: circleSource,
+      style: circleStyles,
+    });
+
+    openLayersMap.addLayer(circleLayer);
+
+
+    // TODO: This is useless for now
+    setMap(openLayersMap);
 
     // Un-Render the map when App is un-rendered
     return () => openLayersMap.setTarget(undefined);
