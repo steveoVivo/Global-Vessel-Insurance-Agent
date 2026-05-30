ffe59926be9849b7536392c26bf5c8e4b03e2630
Steven Shoemaker <stshoema@gmail.com>
2026-05-30 14:04:26 -0700

    More panel styling

--- frontend/src/components/controlPanelComponent.tsx
+++ frontend/src/components/controlPanelComponent.tsx
@@ -23,18 +23,21 @@ function ControlPanelComponent() {
     if (riskDistributionName != 'Custom') return;
     setActivePanel('Custom_Risk');
   }, [riskDistributionName]);
+
+  const customRiskHeaderClassname = 'control-panel-header' + ((activePanel != 'Custom_Risk') ? ' control-panel-header-hoverable' : '');
+  const countryPanelHeaderClassname = 'control-panel-header' + ((activePanel != 'Selected_Country') ? ' control-panel-header-hoverable' : '');
   
 
   return (
     <div className='vessel-control-panel'>
-      <div className='control-panel-container'>
-        <div className='control-panel-header'> Custom Risk {(activePanel == 'Custom_Risk') ? '▲' : '▼'} </div>
+      <div className='control-panel-container' onClick={() => setActivePanel('Custom_Risk')}>
+        <div className={customRiskHeaderClassname}> Custom Risk {(activePanel == 'Custom_Risk') ? '▲' : '▼'} </div>
         <div className='control-panel-content' style={{height: (activePanel == 'Custom_Risk') ? 'auto' : '0px', overflow: 'hidden'}}>
           <CustomriskPanelComponent />
         </div>
       </div>
-      <div className='control-panel-container'>
-        <div className='control-panel-header'> 
+      <div className='control-panel-container' onClick={() => setActivePanel('Selected_Country')}>
+        <div className={countryPanelHeaderClassname}> 
           Selected Country {(activePanel == 'Selected_Country') ? '▲' : '▼'} 
         </div>
         <div className='control-panel-content' style={{display: (activePanel == 'Selected_Country') ? 'block' : 'none', overflow: 'hidden'}}>
